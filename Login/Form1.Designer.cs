f9b7969f00a7a61f1781b951dc3f528662c0b759
BaoDuy-ux <24520377@gm.uit.edu.vn>
2025-10-05 20:46:12 +0700

    first commit

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.bt_login = new System.Windows.Forms.Button();
+            this.bt_signup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(324, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng nhập";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tb_username
+            // 
+            this.tb_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_username.Location = new System.Drawing.Point(314, 117);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(299, 48);
+            this.tb_username.TabIndex = 1;
+            this.tb_username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 40);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên đăng nhập";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(248, 60);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mật khẩu";
+            // 
+            // tb_password
+            // 
+            this.tb_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_password.Location = new System.Drawing.Point(314, 203);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(299, 48);
+            this.tb_password.TabIndex = 4;
+            this.tb_password.UseSystemPasswordChar = true;
+            // 
+            // bt_login
+            // 
+            this.bt_login.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.bt_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_login.Location = new System.Drawing.Point(471, 284);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(184, 54);
+            this.bt_login.TabIndex = 6;
+            this.bt_login.Text = "Đăng nhập";
+            this.bt_login.UseVisualStyleBackColor = false;
+            // 
+            // bt_signup
+            // 
+            this.bt_signup.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.bt_signup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_signup.Location = new System.Drawing.Point(113, 284);
+            this.bt_signup.Name = "bt_signup";
+            this.bt_signup.Size = new System.Drawing.Size(184, 54);
+            this.bt_signup.TabIndex = 7;
+            this.bt_signup.Text = "Đăng ký";
+            this.bt_signup.UseVisualStyleBackColor = false;
+            this.bt_signup.Click += new System.EventHandler(this.bt_signup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(800, 466);
+            this.Controls.Add(this.bt_signup);
+            this.Controls.Add(this.bt_login);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Button bt_login;
+        private System.Windows.Forms.Button bt_signup;
+    }
+}
+
